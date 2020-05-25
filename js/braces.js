046a3c7ef902ba8519ff748d73b601f8f355a19c
Rajath-55 <f20190122@goa.bits-pilani.ac.in>
2020-05-25 20:05:19 +0530

    Move out of a block of code

--- js/braces.js
+++ js/braces.js
@@ -9,7 +9,7 @@ function braceOut(){
   programTextArea.focus();
 
   programTextArea.selectionEnd = programTextArea.selectionEnd - textAfter.length;
-  programTextArea.selectionEnd = programTextArea.selectionEnd + 2;
+  // programTextArea.selectionEnd = programTextArea.selectionEnd + 2;
 
   textBefore = programTextArea.value.substring(0, programTextArea.selectionStart);
   textAfter = programTextArea.value.substring(programTextArea.selectionEnd, programTextArea.value.length);
