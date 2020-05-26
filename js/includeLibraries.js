61bfcaf25f76da59853886dad5db95b42bb38dc9
shreyasnisal <33389081+shreyasnisal@users.noreply.github.com>
2020-05-26 13:18:18 +0530

    Revert "Merge branch 'monaco-editor-cdn' into master"
    This reverts commit 0aac3210140c10a35801d1bfaa63fd85ab654f4c, reversing
    changes made to 94abb79d1369860fc56196d9a04645afcb43c8cf.

--- js/includeLibraries.js
+++ js/includeLibraries.js
@@ -4,13 +4,9 @@ function includeLibrary() {
       libName += splitWords[i].toLowerCase();
   }
 
-  // programTextArea.value = '#include <' + libName + '.h>\n' + textBefore + textAfter;
+  programTextArea.value = '#include <' + libName + '.h>\n' + textBefore + textAfter;
 
-  programTextArea.setValue('#include <' + libName + '.h>\n' + textBefore + textAfter)
-
-  // textBefore = programTextArea.value.substring(0, programTextArea.selectionStart);
-  // textAfter = programTextArea.value.substring(programTextArea.selectionEnd, programTextArea.value.length);
-  textBefore = programTextArea.getValue().substring(0, programTextArea.getSelection().getStartPosition());
-  textAfter = programTextArea.getValue().substring(programTextArea.getSelection().selectionEnd, programTextArea.getValue().length);
+  textBefore = programTextArea.value.substring(0, programTextArea.selectionStart);
+  textAfter = programTextArea.value.substring(programTextArea.selectionEnd, programTextArea.value.length);
   includeStatements += '#include <' + libName + '.h>\n';
 }
--- js/voicerecog.js
+++ js/voicerecog.js
@@ -6,8 +6,7 @@ var recognizing = false;
 var dataTypes = ['int', 'char', 'float', 'double', 'void'];
 var identifiers = {int:'d', float:'f', double:'lf', char:'c'};
 var translationDictionary = {integer:'int', mean:'main', character:'char', '=':'equals', 'percent':'%', 'backslash':'\\', percentage:'%', line:'\\n'};
-// var programTextArea = document.getElementById('program-text-area');
-var programTextArea;
+var programTextArea = document.getElementById('program-text-area');
 var textBefore, textAfter;
 var includeStatements;
 var indentVal = 0;
@@ -19,10 +18,8 @@ var splitWords = [];
 recognition.onresult = function(event) {
     console.log(event.results);
 
-    // textBefore = programTextArea.value.substring(0, programTextArea.selectionStart);
-    // textAfter = programTextArea.value.substring(programTextArea.selectionEnd, programTextArea.value.length);
-    textBefore = programTextArea.getValue().substring(0, programTextArea.getSelection().getStartPosition());
-    textAfter = programTextArea.getValue().substring(programTextArea.getSelection().getEndPosition(), programTextArea.getValue().length);
+    textBefore = programTextArea.value.substring(0, programTextArea.selectionStart);
+    textAfter = programTextArea.value.substring(programTextArea.selectionEnd, programTextArea.value.length);
 
     for (var i = event.resultIndex; i < event.results.length; i++) {
         if (event.results[event.results.length - 1].isFinal) {
@@ -72,3 +69,4 @@ recognition.onresult = function(event) {
     splitWords = [];
 
 }
+
